23dddf3dfff13fd50c86ac38b7a897d0c6b8d4d0
Darshan Rajput <rajputdarshanashok@gmail.com>
2025-02-28 20:08:07 +0530

    Configure webhook for user creation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import { Button } from "@/components/ui/button";
+import { SignInButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
     <div>
       <h1 className="text-2xl font-bold">CodeVerse Home</h1>
+      <SignInButton>
+        <Button>Login</Button>
+      </SignInButton>
     </div>
   );
 }
