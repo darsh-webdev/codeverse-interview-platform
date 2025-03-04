9e82e826473d7fb527b9c6cd63e7cc0a7786d61f
Darshan Rajput <rajputdarshanashok@gmail.com>
2025-03-04 23:16:43 +0530

    [Frontend] feat: Add Navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,7 @@
-import { Button } from "@/components/ui/button";
-import { SignInButton } from "@clerk/nextjs";
-
 export default function Home() {
   return (
     <div>
       <h1 className="text-2xl font-bold">CodeVerse Home</h1>
-      <SignInButton>
-        <Button>Login</Button>
-      </SignInButton>
     </div>
   );
 }
