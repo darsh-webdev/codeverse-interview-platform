841f3674d8900388157760cf9cb2b6a7803b4edb
Darshan Rajput <rajputdarshanashok@gmail.com>
2025-03-05 00:34:59 +0530

    [UI] feat: Update theme color

--- src/components/custom/Navbar.tsx
+++ src/components/custom/Navbar.tsx
@@ -14,8 +14,8 @@ const Navbar = () => {
           href="/"
           className="flex items-center gap-2 font-semibold text-2xl mr-6 font-mono hover:opacity-80 transition-opacity"
         >
-          <CodeIcon className="size-8 text-emerald-500" />
-          <span className="bg-gradient-to-r from-emerald-600 to-teal-500 bg-clip-text text-transparent">
+          <CodeIcon className="size-8 text-violet-700" />
+          <span className="bg-gradient-to-r from-violet-700 to-violet-500 bg-clip-text text-transparent">
             CodeVerse
           </span>
         </Link>
